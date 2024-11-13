01cd899471607fc261d5207f07c00cdb9b216e6e
dsantosgonzalez <santosdiegosd@gmail.com>
2024-11-13 18:13:51 -0500

    5a+b

--- program5B.cpp
+++ program5B.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
+#include <climits>
 /* Solution to program 5B
 * @param n the number of paintings
 * @param W the maximum width of the platform
@@ -8,13 +9,43 @@
 * @param widths the widths of the paintings
 * @return a tuple containing the number of platforms used, the optimal total height, and the number of paintings on each platform
 */
+
+/*
+ * BOTTOM UP!!!!!
+ * OPT[i] = min total height needed for 1 to i
+ * GOAL: OPT[n]
+ *
+ * OPT[i] = min(dp[i], dp[j-1] + max(heights[i to j])
+ */
 std::tuple<int, int, std::vector<int>> program5B(int n, int W, std::vector<int> heights, std::vector<int> widths){
-    /************************
-    * ADD YOUR CODE HERE *
-    ***********************/
-//    return std::make_tuple(0, 0, std::vector<int>()); // replace with your own result.
-    return std::make_tuple(n, 0, heights);
+    std::vector<int> dp(n + 1, INT_MAX); // our dp array, as outlined above. INT_MAX since we are minimizing
+    std::vector<int> platforms(n + 1, 0); // num platforms used up until i
+    std::vector<std::vector<int>> paintingPlaced(n + 1); // how many paintings on each platform
+
+    dp[0] = 0; // base case
+
+    for (int i = 0; i <= n; i++){
+        int totWidth = 0;
+        int maxHeight = 0;
+
+        for (int j = i; j >= 1; j--){
+            totWidth += widths[j - 1];
+            if (totWidth > W)
+                break;
+
+            maxHeight = std::max(maxHeight, heights[j - 1]);
+            if (dp[j - 1] + maxHeight  < dp[i]){
+                dp[i] = dp[j - 1] + maxHeight;
+                platforms[i] = platforms[j - 1] + 1;
+                paintingPlaced[i] = paintingPlaced[j - 1];
+                paintingPlaced[i].push_back(i - j + 1);
+            }
+        }
+    }
+
+    return std::make_tuple(platforms[n], dp[n], paintingPlaced[n]);
 }
+
 int main(){
     int n, W;
     std::cin >> n >> W;
@@ -33,5 +64,5 @@ int main(){
     for(int i = 0; i < std::get<0>(result); i++){
         std::cout << std::get<2>(result)[i] << std::endl;
     }
-    return 0; 
+    return 0;
 }
